c7747d9e43156f11a76a0d918742373c0ef96899
JakubModl <jakubsellw@seznam.cz>
2025-03-09 22:45:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,7 @@ def get_places():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
